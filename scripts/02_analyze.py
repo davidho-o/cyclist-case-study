b1489ce1062b1baf65fb04c6d356959d13f4fd16
davidho-o <davidhora0110@gmail.com>
2026-09-24 14:07:26 +0300

    analyze: further investigate

--- scripts/02_analyze.py
+++ scripts/02_analyze.py
@@ -2,11 +2,20 @@ import pandas as pd
 
 df = pd.read_csv('data/clean_data/data_that_has_been_cleaned.csv')
 
-print("The average length of a bike ride: ")
+print("\nThe average length of a bike ride: ")
 
-print(df.groupby('member_casual')['ride_length'].mean())
+average_length = df.groupby('member_casual')['ride_length'].mean()
+
+print(average_length)
 
 print("\nDays of the week:")
 
-print(df.groupby(['member_casual','day_of_week'])['day_of_week'].count())
+days_of_the_week_use = df.groupby(['member_casual','day_of_week'])['day_of_week'].count()
 
+print(days_of_the_week_use)
+
+busiest_day_members = days_of_the_week_use['member'].idxmax()
+print(f"\nThe busiest day for members is: {busiest_day_members}")
+
+busiest_day_casuals = days_of_the_week_use['casual'].idxmax()
+print(f"\nThe busiest day for casuals is: {busiest_day_casuals}")
